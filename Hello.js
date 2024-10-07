ad04b2010ee93fcd0eb41785dd5fda2fd961dba1
Pulit Chopra <41019851@BSESRPL.RelianceEnergy.com>
2024-10-07 15:16:24 +0530

    Button Created in Main:

--- Hello.js
+++ Hello.js
@@ -5,4 +5,11 @@ function sayName(){
 }
 function isAbove18(age){
     return age>=18;
+
+}
+function newFeatA(){
+    console.log("This is a new feature");
+}
+function clickButton(){
+    console.log("button is being clicked");
 }
